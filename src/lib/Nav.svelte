8cda223a6c0cf6563a9fda839ea1d3b1c11c90af
AhmadShoaib1 <nabias1@student.op.ac.nz>
2023-11-03 11:16:47 +1300

    added a dropdown option for recommendation in the nav bar

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,11 +3,18 @@
 </svelte:head>
 
 <slot />
-<nav>
+
+<nav class="nav">
     <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/character">Characters</a></li>
-      <li><a href="/recommendation">Recommendation</a></li>
-      <li><a href="/">Contact</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/character">Characters</a></li>
+        <li>
+            <a href="/recommendation">Recommendation</a>
+            <ul class="dropdown">
+                <li><a href="/recommendation/option1">Option 1</a></li>
+                <li><a href="/recommendation/option2">Option 2</a></li>
+            </ul>
+        </li>
+        <li><a href="/">Contact</a></li>
     </ul>
 </nav>
