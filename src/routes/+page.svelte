e9d16d92e20d2c39a02e8ba08249e515e294fa87
AhmadShoaib1 <nabias1@student.op.ac.nz>
2023-11-03 22:45:19 +1300

    changed the html but right now the hover effect is not working right

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import Header from "../lib/Header.svelte";
-  letet characters = [
+  let characters = [
     { name: "Shisui", info: "Uchiha clan member" },
     { name: "Madara", info: "Legendary Uchiha" },
     { name: "Itachi", info: "Prodigy Uchiha" },
@@ -8,16 +8,44 @@
     { name: "Minato", info: "Fourth Hokage" },
     { name: "Jiraiya", info: "Legendary Sannin" },
   ];
+  let flipped = Array(characters.length).fill(false);
+
+  function toggleFlip(index) {
+    flipped[index] = !flipped[index];
+  }
 </script>
 
 <Header />
-<p>These are the some of the most liked Naruto characters</p>
-
 <div class="parent">
-  
-  {#each buttons as button (button)}
-    <div class="child">
-      <button class={`image-button ${button}`}></button>
+  {#each characters as { name, info }, index (name)}
+    <div
+      class="child"
+      on:mouseenter={() => toggleFlip(index)}
+      on:mouseleave={() => toggleFlip(index)}
+      on:focus={() => toggleFlip(index)}
+      on:blur={() => toggleFlip(index)}
+      tabindex="0"
+      role="button"
+    >
+      {#if flipped[index]}
+        <div class="image-container flipped">
+          <div class="front">
+            <img src={`../img/${name.toLowerCase()}.jpg`} alt={name} />
+          </div>
+          <div class="back">
+            <p>{info}</p>
+          </div>
+        </div>
+      {:else}
+        <div class="image-container">
+          <div class="front">
+            <img src={`../img/${name.toLowerCase()}.jpg`} alt={name} />
+          </div>
+          <div class="back">
+            <p>{info}</p>
+          </div>
+        </div>
+      {/if}
     </div>
   {/each}
 </div>
