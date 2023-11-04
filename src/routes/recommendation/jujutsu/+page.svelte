e199333f52f8136ac7af48d9cc23f43d2eeb36a2
AhmadShoaib1 <nabias1@student.op.ac.nz>
2023-11-04 21:34:05 +1300

    trying to show the pictures on screen

--- src/routes/recommendation/jujutsu/+page.svelte
+++ src/routes/recommendation/jujutsu/+page.svelte
@@ -1,10 +1,22 @@
 <script>
     let animeList = [
-      { name: "Anime 1", image: "Gojo.jpg", info: "Information about Anime 1" },
-      { name: "Anime 2", image: "Sukuna.jpg", info: "Information about Anime 2" },
-    { name: "Anime 3", image: "nanami.jpg", info: "Information about Anime 3" }
-];
-let additionalInfo = "will put info here";
+        {
+            name: "Anime 1",
+            image: "Gojo.jpg",
+            info: "Information about Anime 1",
+        },
+        {
+            name: "Anime 2",
+            image: "Sukuna.jpg",
+            info: "Information about Anime 2",
+        },
+        {
+            name: "Anime 3",
+            image: "nanami.jpg",
+            info: "Information about Anime 3",
+        },
+    ];
+    let additionalInfo = "will put info here";
 </script>
 
 <svelte:head>
@@ -13,4 +25,18 @@ let additionalInfo = "will put info here";
 
 <div class="additional-info">
     <p>{additionalInfo}</p>
-  </div>
+</div>
+
+<div class="anime-container">
+    {#each animeList as { name, image, info } (name)}
+        <div class="anime-entry">
+            <div class="image-container">
+                <img src={`../img/${image}`} alt={name} />
+            </div>
+            <div class="info-container">
+                <h2>{name}</h2>
+                <p>{info}</p>
+            </div>
+        </div>
+    {/each}
+</div>
