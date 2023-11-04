26a46311b1fd449f22385f1d9c42e05c444f7b27
AhmadShoaib1 <nabias1@student.op.ac.nz>
2023-11-04 21:17:13 +1300

    working on the javascript for the second page

--- src/routes/recommendation/jujutsu/+page.svelte
+++ src/routes/recommendation/jujutsu/+page.svelte
@@ -1,9 +1,12 @@
-<svelte:head>
-  <link rel="stylesheet" href="css/main.css" />
-</svelte:head>
-
 <script>
     let animeList = [
-      { name: "Anime 1", image: "anime1.jpg", info: "Information about Anime 1" }
+      { name: "Anime 1", image: "anime1.jpg", info: "Information about Anime 1" },
+      { name: "Anime 2", image: "anime2.jpg", info: "Information about Anime 2" },
+    { name: "Anime 3", image: "anime3.jpg", info: "Information about Anime 3" }
 ];
+let additionalInfo = "will put info here";
 </script>
+
+<svelte:head>
+    <link rel="stylesheet" href="css/main.css" />
+</svelte:head>
