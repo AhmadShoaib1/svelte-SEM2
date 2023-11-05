1c6ce6cbbaf49ebaf299e556ba33f7d91643b68c
AhmadShoaib1 <nabias1@student.op.ac.nz>
2023-11-05 21:43:36 +1300

    just adding the info for each character

--- src/routes/recommendation/jujutsu/+page.svelte
+++ src/routes/recommendation/jujutsu/+page.svelte
@@ -3,20 +3,30 @@
         {
             name: "Gojo",
             image: "Gojo.jpg",
-            info:[
+            info: [
                 "Limitless Cursed Technique, manipulating space.",
                 "Six Eyes enhance perception, making him unbeatable in combat.",
                 "Combines strategic intelligence with overwhelming strength.",
                 "Wears a blindfold to suppress his power; removing it unleashes immense strength.",
                 "Known as the 'Strongest Jujutsu Sorcerer' in the series.",
                 "Charismatic and humorous personality, a fan favorite.",
-                "Iconic design with white hair and striking blue eyes."
+                "Iconic design with white hair and striking blue eyes.",
             ],
         },
         {
             name: "Sukuna",
             image: "Sukuna.jpg",
-            info: "{Information about Anime 2}",
+            info: [
+                "Sukuna, also known as Ryomen Sukuna, is a major antagonist in 'Jujutsu Kaisen.'",
+                "An ancient and immensely powerful Cursed Spirit, he is feared as the 'King of Curses' with a dark and malevolent reputation.",
+                "Sukuna possesses a collection of powerful Cursed Techniques and immense strength.",
+                "He is divided into twenty fingers, each containing a portion of his power.",
+                "Yuji Itadori, the protagonist, unwittingly consumes one of Sukuna's fingers, leading to a significant turning point in the story.",
+                "Sukuna's presence and influence play a pivotal role in the series' narrative.",
+                "Known for his sadistic and cunning nature, Sukuna is a formidable adversary.",
+                "He has an intricate design with distinctive facial markings and a fearsome demeanor.",
+                "Sukuna's existence adds depth to the series, exploring the consequences of Cursed Energy.",
+            ],
         },
         {
             name: "Nanami",
