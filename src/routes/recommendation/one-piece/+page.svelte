401ab97705b68d3b73f815c098b3db782b15d2d0
AhmadShoaib1 <nabias1@student.op.ac.nz>
2023-11-07 23:02:26 +1300

    started working on one piece page

--- src/routes/recommendation/one-piece/+page.svelte
+++ src/routes/recommendation/one-piece/+page.svelte
@@ -2,6 +2,18 @@
     <link rel="stylesheet" href="css/main.css" />
 </svelte:head>
 
-<div class = "one">
-    hwllo work
+<div class="header-op">
+    <p>
+        " One Piece, created by Eiichiro Oda, is an epic tale of adventure,
+        friendship, and the pursuit of dreams. The story follows Monkey D.
+        Luffy, a spirited young pirate with a dream to become the Pirate King by
+        finding the legendary treasure known as One Piece. Luffy sets sail with
+        his diverse crew, the Straw Hat Pirates, as they navigate the
+        treacherous Grand Line, encounter formidable foes, and forge unbreakable
+        bonds. The series is renowned for its rich world-building, complex
+        characters, and themes of resilience and camaraderie. To embark on this
+        thrilling journey, you can watch One Piece on streaming platforms like
+        Crunchyroll, Funimation, or Hulu, where the Straw Hat Pirates'
+        adventures unfold in all their glory!"
+    </p>
 </div>
