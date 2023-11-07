62849e3ad25e31ec3114165dea6b30557fd2178d
AhmadShoaib1 <nabias1@student.op.ac.nz>
2023-11-07 23:31:01 +1300

    added the html and js for the pictures but does not work- WIP

--- src/routes/recommendation/one-piece/+page.svelte
+++ src/routes/recommendation/one-piece/+page.svelte
@@ -1,9 +1,44 @@
-<svelte:head>
+<script>
+    let characters = [
+      { name: "Monkey D. Luffy", image: "luffy.jpg" },
+      { name: "Roronoa Zoro", image: "zoro.jpg" },
+      { name: "Red-Haired Shanks", image: "shanks.jpg" },
+      { name: "Dracule Mihawk", image: "mihawk.jpg" },
+    ];
+  
+    function createTableRows() {
+      const row = document.getElementById("characterRow");
+  
+      characters.forEach((character) => {
+        const cell = document.createElement("td");
+  
+        const img = document.createElement("img");
+        img.src = character.image;
+        img.alt = character.name;
+  
+        const p = document.createElement("p");
+        p.textContent = character.name;
+  
+        cell.appendChild(img);
+        cell.appendChild(p);
+        row.appendChild(cell);
+      });
+    }
+  
+    // Create table rows on component mount
+    import { onMount } from "svelte";
+    onMount(() => {
+      createTableRows();
+    });
+  </script>
+  
+  <svelte:head>
     <link rel="stylesheet" href="css/main.css" />
-</svelte:head>
-
-<div class="header-op one-piece-section">
-    <p>
+  </svelte:head>
+  
+  <div class="header-op one-piece-section">
+    <div class="description">
+      <p>
         " One Piece, created by Eiichiro Oda, is an epic tale of adventure,
         friendship, and the pursuit of dreams. The story follows Monkey D.
         Luffy, a spirited young pirate with a dream to become the Pirate King by
@@ -15,8 +50,21 @@
         thrilling journey, you can watch One Piece on streaming platforms like
         Crunchyroll, Funimation, or Hulu, where the Straw Hat Pirates'
         adventures unfold in all their glory!"
-    </p>
+      </p>
+    </div>
+  
+    <!-- Table with pictures and names -->
+    <table id="characterTable">
+      <tr id="characterRow"></tr>
+    </table>
+  
+    <!-- Button for watching -->
+
+  </div>
+
+  <div class = "button-op">
     <a href="https://aniwave.to/watch/one-piece.ov8/ep-1" target="_blank">
         <button>Watch Now</button>
-    </a>
-</div>
+      </a>
+  </div>
+  
