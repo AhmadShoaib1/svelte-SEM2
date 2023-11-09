da1048e26a06352d6a4dbd786ca98f82171ea194
Ahmad Nabizada (1000113782) <NABIAS1@student.op.ac.nz>
2023-11-09 15:09:08 +1300

    it opens in new window now, just needed the target

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 
   let characters = [
-    { name: "Shisui", info: "Shisui Uchiha, a shadow among shadows, his eyes echoing the silent whispers of a legacy written in the ethereal ink of the Uchiha clan.", link: "https://www.google.com/search?q=shisui+uchiha" },
-    { name: "Madara", info: "Madara Uchiha, a titan in the tumultuous realm of shinobi, carved his name in history with the indelible strokes of ambition, weaving a tapestry of power and chaos across the ninja world.", link: "https://www.google.com/search?q=madara+uchiha"  },
+    { name: "Shisui", info: "Shisui Uchiha, a shadow among shadows, his eyes echoing the silent whispers of a legacy written in the ethereal ink of the Uchiha clan.", link: "https://www.google.com/search?q=shisui+uchiha", },
+    { name: "Madara", info: "Madara Uchiha, a titan in the tumultuous realm of shinobi, carved his name in history with the indelible strokes of ambition, weaving a tapestry of power and chaos across the ninja world.", link: "https://www.google.com/search?q=madara+uchiha" },
     { name: "Itachi", info: "Itachi Uchiha, a shadow veiled in sacrifice, navigated the labyrinth of darkness to protect the very village that condemned him. His eyes mirrored both tragedy and redemption, leaving an eternal mark on the canvas of ninja lore", link: "https://www.google.com/search?q=itachi+uchiha" },
     { name: "Naruto", info: "Naruto Uzumaki, a spirited hurricane of resilience, carved his legacy with dreams as vast as the Hokage mountain. From the shadows of loneliness, he emerged into a radiant sun, uniting a fractured world with bonds forged in the crucible of his unwavering spirit." , link: "https://www.google.com/search?q=naruto+uzumaki"},
     { name: "Minato", info: "Minato Namikaze, the Flash of the Hidden Leaf, left an indelible mark on history with his unparalleled speed and unyielding love. As the Fourth Hokage, he soared through challenges, leaving a legacy of sacrifice and a flicker of hope in the hearts of those who remember the Yellow Flash." , link: "https://www.google.com/search?q=minato+namikaze"},
@@ -22,6 +22,7 @@ link: "/../img/jiraiya.jpg"
   {#each characters as { name, info, link }, index (name)}
     <a
       href={link}
+      target="_blank"
       class="child"
       on:mouseenter={() => toggleFlip(index)}
       on:mouseleave={() => toggleFlip(index)}
@@ -35,7 +36,6 @@ link: "/../img/jiraiya.jpg"
         </div>
         <div class="back">
           <p>{info}</p>
-
         </div>
       </div>
     </a>
