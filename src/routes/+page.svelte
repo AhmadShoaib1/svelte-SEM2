c55696e7462df688d824b1985963ebfa9e7ced54
Ahmad Nabizada (1000113782) <NABIAS1@student.op.ac.nz>
2023-11-09 14:43:14 +1300

    adding the info for the pics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
   import Header from "../lib/Header.svelte";
   let characters = [
-    { name: "Shisui", info: "Uchiha clan member", link: "/../img/jiraiya.jpg" },
-    { name: "Madara", info: "Legendary Uchiha" },
-    { name: "Itachi", info: "Prodigy Uchiha" },
-    { name: "Naruto", info: "Hokage-to-be" },
+    { name: "Shisui", info: "Shisui Uchiha, a shadow among shadows, his eyes echoing the silent whispers of a legacy written in the ethereal ink of the Uchiha clan."},
+    { name: "Madara", info: "Madara Uchiha, a titan in the tumultuous realm of shinobi, carved his name in history with the indelible strokes of ambition, weaving a tapestry of power and chaos across the ninja world." },
+    { name: "Itachi", info: "Itachi Uchiha, a shadow veiled in sacrifice, navigated the labyrinth of darkness to protect the very village that condemned him. His eyes mirrored both tragedy and redemption, leaving an eternal mark on the canvas of ninja lore" },
+    { name: "Naruto", info: "Naruto Uzumaki, a spirited hurricane of resilience, carved his legacy with dreams as vast as the Hokage mountain. From the shadows of loneliness, he emerged into a radiant sun, uniting a fractured world with bonds forged in the crucible of his unwavering spirit." },
     { name: "Minato", info: "Fourth Hokage" },
     { name: "Jiraiya", info: "Legendary Sannin" },
   ];
