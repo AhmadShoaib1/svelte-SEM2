8c5bd403d640fd251f29265b537fffc227910d27
AhmadShoaib1 <nabias1@student.op.ac.nz>
2023-11-10 21:29:31 +1300

    just fixed the wrong commit

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,15 +1,15 @@
 <nav class="nav">
     <ul>
         <li><a href="/">Home</a></li>
-        <span >Recommendation</span>
         <li><a href="/quotes">Quotes</a></li> 
         <li>
-            <ul class="dropdown">       <!-- Recommendation dropdown with nested links -->
+            <span >Recommendation</span> <!-- Recommendation dropdown with nested links -->
+            <ul class="dropdown">
                 
                 <li><a href="/recommendation/jujutsu">Jujutsu Kaisen</a></li>
                 <li><a href="/recommendation/one-piece">One Piece</a></li>
             </ul>
         </li>
-        <li><a href="/">Contact</a></li>    <!-- Recommendation dropdown with nested links, goes to nowhere as the page for it not done yet, in futur plans -->
+        <li><a href="/">Contact</a></li>    <!-- vontacts, goes to nowhere as the page for it not done yet, in futur plans -->
     </ul>
 </nav>
